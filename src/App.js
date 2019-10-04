8ce9235e9b5ac99235fa37552de75b90256d52d4
Fachi <https://github.com/Fachi3>
2019-10-04 00:09:20 -0500

    Commit first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Header } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+Amplify.configure(aws_exports);
+
+class App extends Component { 
+    render() { 
+        return (
+            <div>
+                <Header as='h1'>Hello World!</Header>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default withAuthenticator(App, {includeGreetings: true});
